737dfd3e65eb5367fd8cb13adc9e83993f0bf68c
Xavier Tan <xavi0007@e.ntu.edu.sg>
2019-03-12 21:40:25 +0800

    scrape skill +10 again, details, flatType, flat price, development name done

--- app/src/main/java/com/example/findmyfirsthome/Controller/HDBDetailsManager.java
+++ app/src/main/java/com/example/findmyfirsthome/Controller/HDBDetailsManager.java
@@ -21,7 +21,8 @@ import org.jsoup.select.Elements;
 
 
 public class HDBDetailsManager extends AsyncTask<String, Void, Void>  {
-    private String url = "http://esales.hdb.gov.sg/bp25/launch/19feb/bto/19FEBBTO_page_6280/$file/about0.html";
+    private String urlDetails = "http://esales.hdb.gov.sg/bp25/launch/19feb/bto/19FEBBTO_page_6280/$file/about0.html";
+    private String urlMain = "http://esales.hdb.gov.sg/bp25/launch/19feb/bto/19FEBBTO_page_6280/$file/about0.html";
     private ProgressDialog mProgressDialog;
 
     public HDBDetailsManager(){
@@ -38,37 +39,34 @@ public class HDBDetailsManager extends AsyncTask<String, Void, Void>  {
 
     //scrap data happens here
     @Override
-    protected Void doInBackground(String... url){
-        try {
+    protected Void doInBackground(String... url) throws IOException {
 
-            ArrayList<String> HDBEstateName = new ArrayList<String>();
-            ArrayList<String> temp;
-            //BoonLay & Jurong west
-            temp = (scrapDevelopmentName(0, 3, 1)); //scrap from table 0, 4th row 2nd data;
-            addToList(temp,HDBEstateName);
-            print(scrapFlatType(0,3,4,8));
-            //SK
-            temp = (scrapDevelopmentName(0, 8, 1));
-            addToList(temp,HDBEstateName);
-            print(scrapFlatType(0,8,9,13));
-            //Kallang
-            temp = (scrapDevelopmentName(0, 14, 1));
-            addToList(temp,HDBEstateName);
-            print(scrapFlatType(0,8,9,13));
+        ArrayList<String> HDBEstateName = new ArrayList<String>();
+        ArrayList<String> temp;
+        //BoonLay & Jurong west
+        temp = (scrapDevelopmentName(urlMain, 0, 3, 1)); //scrap from table 0, 4th row 2nd data;
+        addToList(temp,HDBEstateName);
+        print(scrapFlatType(urlMain,0 ,3,4, 8 ));
+        //SK
+        temp = (scrapDevelopmentName(urlMain, 0, 8, 1));
+        addToList(temp,HDBEstateName);
+        print(scrapFlatType(urlMain,0,8,9, 13));
+        //Kallang
+        temp = (scrapDevelopmentName(urlMain, 0, 4, 1));
+        addToList(temp,HDBEstateName);
+        print(scrapFlatType(urlMain,0,8,9, 13));
+        print(HDBEstateName);
 
-            print(HDBEstateName);
+        description(urlDetails, "Jurong West Jewel", "Boon Lay Glade");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return null;
     }
 
     @Override
     protected void onPostExecute(Void result) {
         mProgressDialog.dismiss();
-        HDBDevelopment HDBD = new HDBDevelopment(ArrayList<HashMap<String, Object>> flatTypeList, String developmentName, String developmentDescription,
-        boolean affordable, LatLng coordinates, ArrayList< MapData > amenities)
+        HDBDevelopment HDBD = new HDBDevelopment(flatTypeList, developmentName, developmentDescription,
+        false, LatLng coordinates, ArrayList<MapData > amenities)
     }
 
 
@@ -91,10 +89,41 @@ public class HDBDetailsManager extends AsyncTask<String, Void, Void>  {
         return true;
     }
 
-    public  HashMap<String, String> scrapFlatType(int tableNumber, int firstRowNumber, int rowStart, int rowEnd){
+    public  void description(String url, String developmentName1, String developmentName2) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        for(int i = 0; i <  document.select("p").size(); i ++) {
+            Element paragraphs = document.select("p").get(i);
+            if(paragraphs.text().length() > 150) {
+                String description = paragraphs.text();
+                if(description.contains(developmentName1)) {
+                    System.out.println(developmentName1 + ": " + description);
+                }else if(description.contains(developmentName2)) {
+                    System.out.println('\n');
+                    System.out.println(developmentName2 + ": " + description);
+                }
+
+            }
+        }
+    }
+
+    //overloading cuz HDB a bitch
+    public  void description(String url, String developmentName1) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        for (int i = 0; i < document.select("p").size(); i++) {
+            Element paragraphs = document.select("p").get(i);
+            if (paragraphs.text().length() > 150) {
+                String description = paragraphs.text();
+                if (description.contains(developmentName1)) {
+                    System.out.println(developmentName1 + ": " + description);
+                }
+            }
+        }
+    }
+
+    public  HashMap<String, String> scrapFlatType(String url, int tableNumber, int firstRowNumber, int rowStart, int rowEnd){
         HashMap<String, String> flatType = new HashMap<String, String>();
         try {
-            Document document = Jsoup.connect("http://esales.hdb.gov.sg/bp25/launch/19feb/bto/19FEBBTO_page_6280/$file/about0.html").get();
+            Document document = Jsoup.connect(url).get();
             Element table = document.select("table").get(tableNumber); //select the first table.
 
             Elements rows = table.select("tr");
@@ -128,10 +157,11 @@ public class HDBDetailsManager extends AsyncTask<String, Void, Void>  {
 
         return flatType;
     }
-    public ArrayList<String> scrapDevelopmentName(int tableNumber, int rowNumber, int colNumber) {
+
+    public ArrayList<String> scrapDevelopmentName(String url, int tableNumber, int rowNumber, int colNumber) {
         try {
             ArrayList<String> textList = new ArrayList<String>();
-            Document document = Jsoup.connect("http://esales.hdb.gov.sg/bp25/launch/19feb/bto/19FEBBTO_page_6280/$file/about0.html").get();
+            Document document = Jsoup.connect(url).get();
 
             Element table = document.select("table").get(tableNumber); //select the first table.
 
