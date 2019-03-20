baa0ce7c141cf630870ac340c3021832f8421292
SoulX <haojie_34@hotmail.com>
2019-03-20 19:17:13 +0800

    ...

--- app/src/main/java/com/example/findmyfirsthome/Controller/StartupControl.java
+++ app/src/main/java/com/example/findmyfirsthome/Controller/StartupControl.java
@@ -1,8 +1,13 @@
 package com.example.findmyfirsthome.Controller;
 
+import com.example.findmyfirsthome.Boundary.DataGovAPI;
+import com.example.findmyfirsthome.Boundary.MapAPI;
+
 public class StartupControl {
     public void run(){
-        MapsController mc1 = new MapsController();
-        HDBDetailsManager HDBdm1 = new HDBDetailsManager();
+        //MapsController mapsController = new MapsController();
+        HDBSplashscreenController SScontrol = new HDBSplashscreenController();
+        //DataGovAPI dataGovAPI = new DataGovAPI();
+
     }
 }
