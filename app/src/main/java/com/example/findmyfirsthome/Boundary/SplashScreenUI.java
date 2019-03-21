8dfbe095353abc62fedfd186c92ccc701b3de99d
ryantiong <tiong_31@hotmail.com>
2019-03-21 16:17:51 +0800

    no message

--- app/src/main/java/com/example/findmyfirsthome/Boundary/SplashScreenUI.java
+++ app/src/main/java/com/example/findmyfirsthome/Boundary/SplashScreenUI.java
@@ -2,8 +2,10 @@ package com.example.findmyfirsthome.Boundary;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.findmyfirsthome.Controller.StartupControl;
 import com.example.findmyfirsthome.R;
 
 public class SplashScreenUI extends AppCompatActivity {
