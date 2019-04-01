5652c61ec2748cdf9ca73da85e81120e76f90f4b
Teesysy <43083112+Teesysy@users.noreply.github.com>
2019-04-01 16:45:02 +0800

    "sy junk"

--- app/src/main/java/com/example/findmyfirsthome/Boundary/AffordabilityReportUI.java
+++ app/src/main/java/com/example/findmyfirsthome/Boundary/AffordabilityReportUI.java
@@ -1,16 +1,22 @@
 package com.example.findmyfirsthome.Boundary;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import com.example.findmyfirsthome.Controller.AffordabilityReportController;
 import com.example.findmyfirsthome.R;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 public class AffordabilityReportUI extends AppCompatActivity {
 
@@ -18,6 +24,7 @@ public class AffordabilityReportUI extends AppCompatActivity {
             tvPropertyname, tvPropertyprice, tvFlattype, tvDownpaymentreq, tvLoanreq, tvRepaymentsum;
 
     private AffordabilityReportController arc;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,22 @@ public class AffordabilityReportUI extends AppCompatActivity {
         tvFlattype.append(flatType);
         setFixedInfo();
         setHDBDependentInfo();
+    }
+
+    //called whenever an item in your options menu is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //R.id.home is the back button at the action bar which is at the top of the app
+            case android.R.id.home:
+                //make it such that action bar's back button which is actually a Up button acts like back button
+                //Up button works by creating new task of the activity instead of actually back to previous activity
+                //onBackPressed() called 1st so the original method of Up button will not be called
+                onBackPressed();
+                return true;
+        }
+
+        return(super.onOptionsItemSelected(item));
     }
 
     private void setFixedInfo(){
@@ -71,5 +94,34 @@ public class AffordabilityReportUI extends AppCompatActivity {
         tvLoanreq.append(HDBDependentInfo.get(2));
         tvRepaymentsum = (TextView) findViewById(R.id.repayment_sum);
         tvRepaymentsum.append(HDBDependentInfo.get(3));
+    }
+
+    private void takeScreenshot(View view) {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+            System.out.println("HERE");
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            //openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or DOM
+            e.printStackTrace();
+        }
     }
 }
