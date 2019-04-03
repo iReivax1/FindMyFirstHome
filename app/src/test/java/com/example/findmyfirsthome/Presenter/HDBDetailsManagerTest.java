8fc726abf728bc91c068f6eeba9b1497d94bdd32
Xavier Tan <xavi0007@e.ntu.edu.sg>
2019-04-03 19:00:08 +0800

    "completed 1 test JUNIT"

--- app/src/test/java/com/example/findmyfirsthome/Presenter/HDBDetailsManagerTest.java
+++ app/src/test/java/com/example/findmyfirsthome/Presenter/HDBDetailsManagerTest.java
@@ -2,11 +2,28 @@ package com.example.findmyfirsthome.Presenter;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class HDBDetailsManagerTest {
 
     @Test
-    public void adaptHDBD() {
+    public void adaptHDBD(ArrayList<String> HDBDevelopmentNames, ArrayList<HashMap<String, Object>> ListFlatType, ArrayList<String> descriptionText, ArrayList<String> ImgURL) {
+        int index = 0;
+        for (String name : HDBDevelopmentNames) {
+            writeHDBDataPlusGetCoord(name, descriptionText.get(index), ImgURL.get(index), false);
+            for(int i=0;i<ListFlatType.size();i++) {
+                HashMap<String, Object> ftNew = new HashMap<String, Object>();
+                for(String ft : ListFlatType.get(i).keySet()) {
+                    ftNew.put(ft,ListFlatType.get(i).get(ft));
+                }
+                writeHDBFlatData(name, ftNew);
+            }
+            index++;
+        }
+
+        return true;
     }
 }
