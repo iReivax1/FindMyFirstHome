a8d5cea23ccefd995bd5820890ea8816580faebb
Xavier Tan <xavi0007@e.ntu.edu.sg>
2019-04-03 16:16:22 +0800

    "test classes added"

---
+++ app/src/test/java/com/example/findmyfirsthome/Presenter/CalculateProfileControlTest.java
@@ -0,0 +1,13 @@
+package com.example.findmyfirsthome.Presenter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculateProfileControlTest {
+
+    @Test
+    public void setAffordability() {
+    }
+
+}
---
+++ app/src/test/java/com/example/findmyfirsthome/Presenter/HDBDetailsManagerTest.java
@@ -0,0 +1,12 @@
+package com.example.findmyfirsthome.Presenter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HDBDetailsManagerTest {
+
+    @Test
+    public void adaptHDBD() {
+    }
+}
